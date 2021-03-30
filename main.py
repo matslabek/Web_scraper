4b5d93ea166e8bc5007cba9a4886ccd4989f063c
Mats <matslabek@gmail.com>
2021-03-30 21:49:59 +0200

    Movie scraping functionality

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def web_scraper():
     user_url = input("Input the URL:")
     response = request_getter(user_url)
+    if response:
+        print(response)
 
 
 def request_getter(url):
-    r = requests.get(url)
+    r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
     if r.status_code != 200:
-        print("Invalid quote resource!")
+        print("Invalid movie page!")
         return 0
-    content = r.json()['content']
-    if content:
-        print(content)
-    else:
-        print("Invalid quote resource!")
+    soup = BeautifulSoup(r.content, 'html.parser')
+    try:
+        if soup.find("meta", attrs={"property": "og:site_name"})['content'] != 'IMDb':
+            print("Invalid movie page!")
+            return 0
+    except TypeError:
+        print('Invalid movie page!')
         return 0
-    return r
+    accepted_types = ['video.movie', 'video.tv_show']
+    if soup.find("meta", attrs={"property": "og:type"})['content'] not in accepted_types:
+        print("Invalid movie page!")
+        return 0
+    title = soup.find("meta", attrs={'name': 'title'})
+    description = soup.find("meta", attrs={'name': 'description'})
+
+    return {"title": title['content'], "description": description['content']}
 
 
-web_scraper()
+def main():
+    web_scraper()
+
+
+if __name__ == '__main__':
+    main()
